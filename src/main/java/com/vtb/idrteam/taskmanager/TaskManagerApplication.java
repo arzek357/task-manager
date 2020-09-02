c0df74cea87875e626bc1435ed857bdcb01e065b
arzek357 <arzek357@gmail.com>
2020-09-02 19:49:07 +0300

    Add Dev Branch

--- src/main/java/com/vtb/idrteam/taskmanager/TaskManagerApplication.java
+++ src/main/java/com/vtb/idrteam/taskmanager/TaskManagerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskManagerApplication {
-
+	//Add Deb Branch
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagerApplication.class, args);
 	}
