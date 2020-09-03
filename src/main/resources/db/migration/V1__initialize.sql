53b1e1a2d6d3f20d8faef266fc840672ac938e7b
stamacake <ufigroman@gmail.com>
2020-09-03 20:37:06 +0300

    furniture

--- src/main/resources/db/migration/V1__initialize.sql
+++ src/main/resources/db/migration/V1__initialize.sql
@@ -11,3 +11,17 @@ values
 ('Banana','Yellow fruit',50),
 ('Apple','Round fruit',40),
 ('Tomato','Red fruit',20);
+
+create table furniture (
+  id          bigserial,
+  title       varchar(255) not null,
+  type        varchar(255) not null,
+  size        int not null,
+  primary key (id)
+);
+
+insert into furniture (title, type, size)
+values
+('Adde', 'Chair', 1000),
+('Linnmon', 'Table', 700),
+('Neiden','Bed', 500);
