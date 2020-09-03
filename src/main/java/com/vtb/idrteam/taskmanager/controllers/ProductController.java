2e39c59e12d2a11de92bd8116ddc789b563c9571
arzek357 <arzek357@gmail.com>
2020-09-03 21:19:39 +0300

    Add new methods in ProductController

--- src/main/java/com/vtb/idrteam/taskmanager/controllers/ProductController.java
+++ src/main/java/com/vtb/idrteam/taskmanager/controllers/ProductController.java
@@ -1,11 +1,11 @@
 package com.vtb.idrteam.taskmanager.controllers;
 
+import com.vtb.idrteam.taskmanager.entities.Furniture;
 import com.vtb.idrteam.taskmanager.entities.Product;
 import com.vtb.idrteam.taskmanager.services.ProductService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +19,22 @@ public class ProductController {
     @GetMapping
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getById(@PathVariable Long id){
+        return productService.getById(id).orElseThrow(() -> new RuntimeException("No product with id = " + id));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        productService.deleteById(id);
+    }
+
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product create(@RequestBody Product product){
+        return productService.saveOrUpdate(product);
     }
 
 }
--- src/main/java/com/vtb/idrteam/taskmanager/services/ProductService.java
+++ src/main/java/com/vtb/idrteam/taskmanager/services/ProductService.java
@@ -1,11 +1,13 @@
 package com.vtb.idrteam.taskmanager.services;
 
+import com.vtb.idrteam.taskmanager.entities.Document;
 import com.vtb.idrteam.taskmanager.entities.Product;
 import com.vtb.idrteam.taskmanager.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -18,5 +20,21 @@ public class ProductService {
 
     public List<Product> getAllProducts(){
         return productRepository.findAll();
+    }
+
+    public Optional<Product> getById(Long id){
+        return productRepository.findById(id);
+    }
+
+    public Product saveOrUpdate(Product product){
+        return productRepository.save(product);
+    }
+
+    public void deleteById(Long id){
+        productRepository.deleteById(id);
+    }
+
+    public boolean existsById(Long id) {
+        return productRepository.existsById(id);
     }
 }
