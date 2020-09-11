c4942b2d49a78946a4776bf2199e50aa7438457e
arzek357 <arzek357@gmail.com>
2020-09-11 12:18:43 +0300

    Refactor Task entity and add new front logic for visualisation tasks

--- src/main/resources/db/migration/V2__inserts.sql
+++ src/main/resources/db/migration/V2__inserts.sql
@@ -24,9 +24,9 @@ insert into taskstates (codename, name) values
 
 insert into tasksauthorities (name) values ('creator'), ('performer'), ('subscriber');
 
--- insert into tasks (name, description, project_id, archived, deadline_time) values
--- ('task1', 'desc1', 1, false, current_timestamp+5),
--- ('task2', 'desc2', 1, true, current_timestamp +7);
+insert into tasks (name, description, project_id, archived, deadline_time,state) values
+('task1', 'desc1', 1, false, current_timestamp+5,'CREATED'),
+('task2', 'desc2', 1, true, current_timestamp +7,'CREATED');
 --
 -- insert into tasksauthorities (name) values ('worker');
 -- insert into taskparticipants (task_id, user_id, task_authority_id) values
