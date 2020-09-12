2adf9afdafc6f25b114b0609809678c9febd499c
FMW14 <fmw14@yandex.ru>
2020-09-12 18:29:03 +0300

    added more validation for UserDto

--- src/main/java/com/vtb/idrteam/taskmanager/entities/dtos/UserDto.java
+++ src/main/java/com/vtb/idrteam/taskmanager/entities/dtos/UserDto.java
@@ -4,15 +4,18 @@ import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class UserDto {
     @Length(min = 2, max = 30, message = "Username must contain from 2 to 30 characters")
     private String username;
 
-    //todo min 5 chars for password
+    @Length(min = 5)
     private String password;
 
+    @Length(min = 5)
     private String passwordConfirm;
 
     @Length(min = 1, max = 30, message = "Name must contain from 1 to 30 characters")
@@ -21,6 +24,8 @@ public class UserDto {
     @Length(min = 1, max = 30, message = "Surname must contain from 1 to 30 characters")
     private String surname;
 
+    @NotNull
+    @NotEmpty
     @Email
     private String email;
 }
